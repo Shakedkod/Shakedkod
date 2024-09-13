4d5b0973500800b4e54be46ace0f1ad4107ccc23
ShakedKod <ShakedKod@gmail.com>
2024-09-13 21:53:01 +0300

    vault backup: 2024-09-13 21:53:01
    Affected files:
    .obsidian/plugins/obsidian-latex-suite/main.js
    .obsidian/plugins/obsidian-latex-suite/manifest.json
    .obsidian/plugins/recent-files-obsidian/data.json
    00 - School/מדעי המחשב/כיתה יב/SummerWork/src/Quadrilateral.java

--- 00 - School/מדעי המחשב/כיתה יב/SummerWork/src/Quadrilateral.java
+++ 00 - School/מדעי המחשב/כיתה יב/SummerWork/src/Quadrilateral.java
@@ -2,28 +2,16 @@ public class Quadrilateral
 {
     private Point p1;
     private Point p2;
-    private Point p3;
 
-    public Quadrilateral(Point p1, Point p2, Point p3)
+    public Quadrilateral(Point p1, Point p2)
     {
         this.p1 = new Point(p1);
         this.p2 = new Point(p2);
-        this.p3 = new Point(p3);
     }
 
     public double area()
     {
-        //TODO: fix this
-        if (p1.getX() == p2.getX())
-        {
-            if (p1.getY() == p3.getY())
-                return (p1.distance(p2) * p1.distance(p3));
-            return (p1.distance(p2) * p2.distance(p3));
-        }
-
-        if (p1.getY() == p2.getY())
-            return (p1.distance(p2) * p2.distance(p3));
-        return (p1.distance(p3) * p3.distance(p2));
+        return (p2.getX() - p1.getX()) * (p1.getY() - p2.getY());
     }
 
     public boolean isSquare()
@@ -49,11 +37,6 @@ public class Quadrilateral
         return new Point(p2);
     }
 
-    public Point getPoint3()
-    {
-        return new Point(p3);
-    }
-
     public void setPoint1(Point newP1)
     {
         this.p1 = new Point(newP1);
@@ -62,11 +45,6 @@ public class Quadrilateral
     public void setPoint2(Point newP2)
     {
         this.p2 = new Point(newP2);
-    }
-
-    public void setPoint3(Point newP3)
-    {
-        this.p3 = new Point(newP3);
     }
 
     @Override
